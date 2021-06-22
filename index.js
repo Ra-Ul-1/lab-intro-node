cd081eabc46b2ef4bbf0481a78db77a46fa46062
Raoul Ullens <raoulullens@live.com>
2021-06-22 15:00:49 +0200

    add iterations 1-5

--- index.js
+++ index.js
@@ -1,17 +1,44 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = this.items.length;
+  }
 
-  add(item) {}
+  add(item) {
+    this.items.push(item);
+    this.length += 1;
+    this.items.sort(function(a, b) {return a-b});
+    };
 
-  get(pos) {}
+  get(pos) {
+    if (pos >= this.length) {
+      // error is a constructor
+      throw new Error("Out of bounds")
+    } else {
+      return this.items[pos];
+  }      
+}
 
-  max() {}
+  max() {
+    if (this.length != 0) {
+      return Math.max(...this.items)
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
-  min() {}
+  min() {
+    if (this.length != 0) {
+      return Math.min(...this.items)
+    } else {
+      throw new Error("EmptySortedList");
+    }
+  }
 
   sum() {}
 
   avg() {}
 }
+
 
 module.exports = SortedList;
